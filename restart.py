cf5ce99130a4facd225265de0a5ebf26efd701ac
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-03 22:08:15 +0530

    Add files via upload

---
+++ restart.py
@@ -0,0 +1,28 @@
+import os
+
+restart = input("Do u want to restart ur system ? (y / n) : ")
+if restart == 'n':
+    exit()
+else:
+    os.system("shutdown /r")
+
+
+
+
+
+
+
+
+import os
+
+shutdown = input("Do u want to shutdown ur system ? (y / n) : ")
+if shutdown == 'n':
+    exit()
+else:
+    os.system("shutdown /s /t")
+
+
+
+
+
+    
