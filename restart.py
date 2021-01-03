a86e52e845ce1f071ef969232d3fe1db5bfc3087
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-03 22:09:05 +0530

    Update restart.py

--- restart.py
+++ restart.py
@@ -4,25 +4,4 @@ restart = input("Do u want to restart ur system ? (y / n) : ")
 if restart == 'n':
     exit()
 else:
-    os.system("shutdown /r")
-
-
-
-
-
-
-
-
-import os
-
-shutdown = input("Do u want to shutdown ur system ? (y / n) : ")
-if shutdown == 'n':
-    exit()
-else:
-    os.system("shutdown /s /t")
-
-
-
-
-
-    
+    os.system("shutdown /r /t")
